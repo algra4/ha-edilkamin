acae20a0a6e141ad15c98087a1a568575c165f75
Alexis Gramaglia <algr@hms.se>
2026-01-01 21:22:16 +0100

    fixup! feat: Add auto preset mode for fan and climate entities

--- custom_components/edilkamin/fan.py
+++ custom_components/edilkamin/fan.py
@@ -24,7 +24,7 @@ _LOGGER = logging.getLogger(__name__)
 
 SPEED_RANGE = (1, 5)  # away is not included in speeds and instead mapped to off
 
-PRESET_MODE_AUTO = "auto"
+PRESET_MODE_AUTO = "Auto"
 PRESET_MODES = [PRESET_MODE_AUTO]
 
 async def async_setup_entry(hass, config_entry, async_add_devices):
